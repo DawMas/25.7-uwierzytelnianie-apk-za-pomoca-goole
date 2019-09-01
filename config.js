10adf78df2a74dd4d027a1f1d0f904abd1183518
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-09-01 12:35:08 +0200

    Add google authorization

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '18831622850-6lrq89mjdgspk0cod1iqnmheo748vidq.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '9BRXMFbTfriKUFE0c0ZPhFrW',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+
+};
